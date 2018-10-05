c0ef0be4aeff8eb2875118ce97f6cc09104f6523
Γκάγκας Αναστάσιος <tasosggps@gmail.com>
2018-10-06 02:56:04 +0300

    Class for assistance in sorting empty and not empty blocks

---
+++ src/Block.java
@@ -0,0 +1,22 @@
+
+public class Block {
+	
+	private int row, col, number;
+	
+	public Block(int r, int c) {
+		row = r;
+		col = c;
+	}
+	
+	public int row() {
+		return row;
+	}
+	
+	public int col() {
+		return col;
+	}
+	
+	public int number() {
+		return number;
+	}
+}
